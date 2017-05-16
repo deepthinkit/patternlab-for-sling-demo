b2ac1b88410441f1d0e662df36e139dc4bc20de2
Kamil <kamilc89@gmail.com>
2017-05-16 14:56:35 +0200

    Translating the rest of patterns from mustache to htl, changes in pom, changing data files extensions to json

--- patternlab-demo-apps/src/main/resources/jcr_root/apps/patternlab-demo/atoms/buttons/button.js
+++
@@ -1,1 +0,0 @@
-{"btnClass" : "", "btnLink" : "#", "btnText" : "Button"}
--- patternlab-demo-apps/src/main/resources/jcr_root/apps/patternlab-demo/templates/dashboard.hacked.js
+++
@@ -1,6 +0,0 @@
-{
-	"alert" : {
-		"styleModifier" : "error"
-	},
-	"alertText" : "On May 22nd, hackers from a hidden underground tunnel somewhere in Siberia hacked our servers and compromised all of our sensitive data. <a href='#'> Please reset your password immediately!</a>"
-}
