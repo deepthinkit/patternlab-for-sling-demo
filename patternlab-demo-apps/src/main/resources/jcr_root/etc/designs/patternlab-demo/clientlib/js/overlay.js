9ba5e19c4f7b321ad51e202d6afe34db1dc6ef79
Kamil Ciecierski <kamilc89@gmail.com>
2017-09-25 00:38:17 +0200

    Removing not needed JS, small tweaks in scripts structure to match gulp configuration

--- patternlab-demo-apps/src/main/resources/jcr_root/etc/designs/patternlab-demo/clientlib/js/overlay.js
+++
@@ -1,6 +0,0 @@
-var overlayBtn = document.querySelectorAll('.c-btn--overlay');
-for (i = 0; i < overlayBtn.length; i++) {
-  overlayBtn[i].addEventListener('click', function() {
-  	this.parentNode.classList.add("c-overlay--is-hidden");
-  });
-}
